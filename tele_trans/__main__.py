e709f7dac80f5306ece1755efaf6fc721f0abfbd
honanaipr <honanaipr@gmail.com>
2023-09-20 21:40:13 +0300

    startup logging

--- tele_trans/__main__.py
+++ tele_trans/__main__.py
@@ -4,25 +4,32 @@ from asyncio import run
 from aiogram.filters import CommandStart
 from aiogram.types import Message
 from .routers.register import register_router
-
-i18n = I18n(path="tele_trans/locales", default_locale="en", domain="messages")
+from loguru import logger
 
 dp = Dispatcher()
 bot = Bot(token="6230284749:AAFAeqz29Afj6PA2TXinqmj2wHZD09JfFLk")
 
+i18n = I18n(path="tele_trans/locales", default_locale="en", domain="messages")
 dp.message.middleware(SimpleI18nMiddleware(i18n))
 
 
 @dp.message(CommandStart())
 async def message_handler(message: Message, i18n: I18n):
     _ = i18n.gettext
-    await message.answer(_("message_id"))
+    await message.answer(_("Banner"))
 
 
 dp.include_router(register_router)
 
+@dp.startup()
+async def on_start():
+    bot_user = await bot.me()
+    logger.info(f"bot started. botname: {bot_user.username}")
+
+
 
 async def main():
+    logger.info("starting ...")
     await dp.start_polling(bot)
 
 
